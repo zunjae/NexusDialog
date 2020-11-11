9a07a3d8c3a73bb56aaf08449de048afbfb01714
sunjay <9930542+zunjae@users.noreply.github.com>
2020-11-11 13:56:36 +0100

    public

--- nexusdialog/src/main/java/com/github/dkharrat/nexusdialog/FormModel.java
+++ nexusdialog/src/main/java/com/github/dkharrat/nexusdialog/FormModel.java
@@ -2,6 +2,7 @@ package com.github.dkharrat.nexusdialog;
 
 import java.beans.PropertyChangeListener;
 import java.beans.PropertyChangeSupport;
+import java.util.Objects;
 
 /**
  * <code>FormModel</code> is an abstract class that represents the backing data for a form. It provides a mechanism
@@ -10,41 +11,44 @@ import java.beans.PropertyChangeSupport;
 public abstract class FormModel {
     private final PropertyChangeSupport propertyChangeSupport = new PropertyChangeSupport(this);
 
+    private static boolean objectsEqual(Object a, Object b) {
+        return Objects.equals(a, b);
+    }
+
     /**
      * This method is called when a form element changes its value through user input or external changes. Subclasses
      * must implement this method to update the backing model.
      *
-     * @param name      the field name to set the value for
-     * @param newValue  the value to set
+     * @param name     the field name to set the value for
+     * @param newValue the value to set
      */
-    protected abstract void setBackingValue(String name, Object newValue);
+    public abstract void setBackingValue(String name, Object newValue);
 
     /**
      * This method is called whenever the form needs the current value for a specific field. Subclasses must implement
      * this method to provide the form access to the backing model.
      *
-     * @param name      the field name to retrieve the value for
-     * @return          the current value of the specified field
+     * @param name the field name to retrieve the value for
+     * @return the current value of the specified field
      */
-    protected abstract Object getBackingValue(String name);
+    public abstract Object getBackingValue(String name);
 
     /**
      * Returns the value for the specified field name.
      *
-     * @param name  the field name
-     * @return      the value currently set for the specified field name
+     * @param name the field name
+     * @return the value currently set for the specified field name
      */
     public final Object getValue(String name) {
         return getBackingValue(name);
     }
 
     /**
-     *
      * Sets a value for the specified field name. A property change notification is fired to registered listeners if
      * the field's value changed.
      *
-     * @param name      the field name to set the value for
-     * @param newValue  the value to set
+     * @param name     the field name to set the value for
+     * @param newValue the value to set
      */
     public final void setValue(String name, Object newValue) {
         Object curValue = getBackingValue(name);
@@ -52,11 +56,6 @@ public abstract class FormModel {
             setBackingValue(name, newValue);
             propertyChangeSupport.firePropertyChange(name, curValue, newValue);
         }
-    }
-
-    private static boolean objectsEqual(Object a, Object b) {
-        return a == b || (a != null && a.equals(b));
-
     }
 
     /**
